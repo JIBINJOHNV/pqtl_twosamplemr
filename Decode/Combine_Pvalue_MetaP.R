4887be6decda4128472affed13f64553a312e2d9
JIBINJOHNV <69025183+JIBINJOHNV@users.noreply.github.com>
2023-08-01 17:46:15 -0400

    Update Combine_Pvalue_MetaP.R

--- Decode/Combine_Pvalue_MetaP.R
+++ Decode/Combine_Pvalue_MetaP.R
@@ -1,6 +1,45 @@
 library(metap)
 library(plyr)
 
+
+df=read.csv("Biogen_Decode_pQTL.csv")
+scolumns<-c("Decode_MRIVWtest_Pvalue","Biogen_MRIVWtest_Pvalue")
+df3<-df[,scolumns]
+
+metap_values1<-vector()
+
+for(indx in 1:nrow(df3)) {
+  teest<-unlist(as.vector(df3[indx,]))
+  metap_values1<-c(metap_values1,allmetap(teest,method="sumlog")$p[[1]] )
+}
+
+
+
+scolumns<-c("Decode_MR_Pipeline_pval","Biogen_MR_Pipeline_pval")
+df3<-df[,scolumns]
+
+metap_values2<-vector()
+
+for(indx in 1:nrow(df3)) {
+  teest<-unlist(as.vector(df3[indx,]))
+  metap_values2<-c(metap_values2,allmetap(teest,method="sumlog")$p[[1]] )
+}
+
+df$metap_MRIVWtest<-metap_values1
+df$metap_MR_Pipeline_pval<-metap_values2
+
+
+write.csv(df, "Biogen_Decode_pQTL_Meetap2.csv", row.names = FALSE)
+
+
+
+
+############################################
+
+
+
+
+
 df=read.csv("Biogen_Decode_pQTL.csv")
 scolumns<-c("Decode_MRIVWtest_Pvalue","Biogen_MRIVWtest_Pvalue")
 
