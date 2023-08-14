dd63390c4d1239760b10daeba6d2988a6af5b7c9
JIBINJOHNV <69025183+JIBINJOHNV@users.noreply.github.com>
2023-08-14 16:39:16 -0400

    Update pgc3_SCZ_sumstat_to_vcf.py

--- OuctcomeGWAS/pgc3_SCZ_sumstat_to_vcf.py
+++ OuctcomeGWAS/pgc3_SCZ_sumstat_to_vcf.py
@@ -88,23 +88,33 @@ os.system(f'''bcftools annotate --threads  10 \
     -o {path}/{filename[:-4]}_GRCh38_rsid156.vcf.gz -O z {path}/{filename[:-4]}_GRCh38.vcf.gz''')
 
 os.system(f'tabix -f -p vcf {path}/{filename[:-4]}_GRCh38_rsid156.vcf.gz')
-os.system(f"rm {path}/{filename[:-4]}_GRCh38.vcf.gz* {path}/{filename[:-4]}_GRCh38.vcf.gz*")
-os.system(f"rm {path}/{filename[:-4]}.vcf.gz*")
+os.system(f"rm {path}/{filename[:-4]}_GRCh38.vcf.gz* {path}/{filename[:-4]}.vcf.gz")
 
 ##Ading dbsnp 156 to the format section
 os.system(f'zgrep -v "##" {path}/{filename[:-4]}_GRCh38_rsid156.vcf.gz > {path}/{filename[:-4]}_GRCh38_rsid156.tab')
 os.system(f'zgrep  "##" {path}/{filename[:-4]}_GRCh38_rsid156.vcf.gz > {path}/{filename[:-4]}_GRCh38_rsid156_header.txt')
 
+
 df=pd.read_csv(f"{path}/{filename[:-4]}_GRCh38_rsid156.tab",sep="\t")
+format_df2=df["FORMAT"].str.split(":",expand=True)
+format_df2[7]="ID"
+df["FORMAT"]=format_df2[0]+":"+format_df2[1]+":"+format_df2[2]+":"+format_df2[3]+":"+format_df2[4]+":"+format_df2[5]+":"+format_df2[6]+":"+format_df2[7]
+
 format_df=df[ID].str.split(":",expand=True)
 df["ID"]=np.where(df["ID"].str.contains("rs"),df["ID"],df["#CHROM"].astype(str)+"_"+df["POS"].astype(str)+"_"+df["REF"]+"_"+df["ALT"])
 format_df[7]=df["ID"]
 format_df=format_df.astype("str")
 df[ID]=format_df[0]+":"+format_df[1]+":"+format_df[2]+":"+format_df[3]+":"+format_df[4]+":"+format_df[5]+":"+format_df[6]+":"+format_df[7]
+
+df["FORMAT"].str.split(":",expand=True).isna().sum()
+df[ID].str.split(":",expand=True).isna().sum()
+
 df.to_csv(f"{path}/{filename[:-4]}_GRCh38_rsid156.tab",sep="\t",index=None)
 
 os.system(f"cat {path}/{filename[:-4]}_GRCh38_rsid156_header.txt {path}/{filename[:-4]}_GRCh38_rsid156.tab | bgzip -c > {path}/{filename[:-4]}_GRCh38_rsid156.vcf.gz ")
 os.system(f'tabix -f -p vcf  {path}/{filename[:-4]}_GRCh38_rsid156.vcf.gz')
+
+
 
 ##Ading UniqID to the format section
 df["ID"]=df["#CHROM"].astype(str)+"_"+df["POS"].astype(str)+"_"+df["REF"]+"_"+df["ALT"]
