3e8b48f2685883e0830496822dc2c5f445b64fb4
JIBINJOHNV <69025183+JIBINJOHNV@users.noreply.github.com>
2023-08-14 19:26:24 -0400

    Update Biogen_CisExposureUniqVariants_twosampleMR_Running.R

--- Biogen/UniqVariantID_BiogenReference/MR_Analysis/Biogen_CisExposureUniqVariants_twosampleMR_Running.R
+++ Biogen/UniqVariantID_BiogenReference/MR_Analysis/Biogen_CisExposureUniqVariants_twosampleMR_Running.R
@@ -323,3 +323,20 @@ write.csv(mendelianpipeline_mrresult, glue('{prefix}_MendelianPipelineTest.csv')
 write.csv(MendelianRandomization_df, glue('{prefix}_MendelianRandomization_AllTest.csv'),row.names = FALSE)
 write.csv(mr_presso_df, glue('{prefix}_MendelianRandomization_Presso_Test.csv'),row.names = FALSE)
 write.csv(mr_ivw_delta_df, glue('{prefix}_MendelianRandomization_IVW_Delta_Test.csv'),row.names = FALSE)
+
+#!/bin/bash
+
+#SBATCH --ntasks=4
+#SBATCH --time=00:01:00
+#SBATCH --mem=90GB
+#SBATCH --partition=cpu
+#SBATCH --time=200:00:00
+
+
+#source /home/jjohn1/modulefiles/anaconda3/bin/activate
+#module load libxml2/2.9.8-GCCcore-6.4.0
+#module load OpenSSL/1.1
+#module load R/4.1.3-foss-2021b
+#module load GMP/6.2.1-GCCcore-11.2.0
+
+#Rscript Biogen_CisExposureUniqVariants_twosampleMR_Running.R
