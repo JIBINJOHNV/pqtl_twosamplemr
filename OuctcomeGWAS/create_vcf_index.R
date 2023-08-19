4055e53e7a26e6fb8475cc0f069a1a44018e8325
JIBINJOHNV <69025183+JIBINJOHNV@users.noreply.github.com>
2023-08-18 22:20:25 -0400

    Update create_vcf_index.R

--- OuctcomeGWAS/create_vcf_index.R
+++ OuctcomeGWAS/create_vcf_index.R
@@ -24,7 +24,8 @@ suppressWarnings(suppressPackageStartupMessages({
 
 outcome_gwassumstat <- c("ADHD2022_iPSYCH_deCODE_PGC.meta_GRCh38_UniqID.vcf.gz","iPSYCH-PGC_ASD_Nov2017_GRCh38_UniqID.vcf.gz",
                          "PGC_UKB_depression_genome-wide_GRCh38_UniqID.vcf.gz","daner_bip_pgc3_nm_noukbiobank_GRCh38_UniqID.vcf.gz",
-                         "PGC3_SCZ_wave3.european.autosome.public.v3_GRCh38_UniqID.vcf.gz")
+                         "PGC3_SCZ_wave3.european.autosome.public.v3_GRCh38_UniqID.vcf.gz",
+                        "daner_MDDwoBP_20201001_2015iR15iex_HRC_MDDwoBP_iPSYCH2015i_Wray_FinnGen_MVPaf_2_HRC_MAF01_GRCh38_UniqID.vcf.gz")
 
 path <- "/mnt/depts/dept04/human_genetics/users/jjohn1/Outcome_GWAS/All_Processed_Outcome_GWAS_UniqIds/"
 
