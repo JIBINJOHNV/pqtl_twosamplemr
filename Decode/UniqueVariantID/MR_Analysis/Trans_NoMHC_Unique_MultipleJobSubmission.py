26e9c8a19197f94e377d772a19ae604023c73d8e
JIBINJOHNV <69025183+JIBINJOHNV@users.noreply.github.com>
2023-10-03 10:01:29 -0400

    Update Trans_NoMHC_Unique_MultipleJobSubmission.py

--- Decode/UniqueVariantID/MR_Analysis/Trans_NoMHC_Unique_MultipleJobSubmission.py
+++ Decode/UniqueVariantID/MR_Analysis/Trans_NoMHC_Unique_MultipleJobSubmission.py
@@ -14,7 +14,7 @@ df=pd.read_csv(file)
 df=df.sort_values(by=["exposure","ID"]).reset_index(drop="index")
 exposure_list=list(df["exposure"].unique())
 
-filename_dict={"daner_PGC_SCZ_w3_90_0418b_ukbbdedupe.trios_GRCh38_UniqID.vcf.gz":"PGC3_SCZ_NoUKB",
+filename_dict={"Cognition_Meta_GWAS_without_UKBPP1_GRCh38_UniqID.vcf.gz":"Cognition","daner_PGC_SCZ_w3_90_0418b_ukbbdedupe.trios_GRCh38_UniqID.vcf.gz":"PGC3_SCZ_NoUKB",
               "ADHD2022_iPSYCH_deCODE_PGC.meta_GRCh38_UniqID.vcf.gz":"PGC_ADHD2022_iPSYCH_deCODE",
               "daner_bip_pgc3_nm_noukbiobank_GRCh38_UniqID.vcf.gz":"BIP_PGC3_noukb",
               "iPSYCH-PGC_ASD_Nov2017_GRCh38_UniqID.vcf.gz":"ASD_PGC",
@@ -26,19 +26,16 @@ result_df=pd.DataFrame()
 
 for rscript in rscripts:
     r_prefix=rscript.replace("_MRAnalysis_Running.R","")
-    
     for file,name in filename_dict.items():
         file=file.replace(".gz","")
         os.system(f'''sed '/prefix="cis_exposure"/c\prefix="{pqtltype}_{name}_{r_prefix}"' {rscript} > {pqtltype}_{name}_{rscript}''')
         os.system(f'''sed -i 's/PGC3_SCZ_wave3.european.autosome.public.v3_GRCh38_UniqID.vcf/{file}/g' {pqtltype}_{name}_{rscript} ''')
         os.system(f'''sed 's/Rscript/Rscript {pqtltype}_{name}_{rscript}/' MRAnalysis_sbatch_command.sh > {pqtltype}_{name}_{r_prefix}_MRAnalysis_sbatch_command.sh ''')
-
         n=0
         for i in range(0,50):
             temp_df=df[df["exposure"].isin(exposure_list[n:n+41])]
             result_df=pd.concat([temp_df,result_df])
             n=n+41
-            
             Dir=f"{basedir}Part{i}/"
             temp_df.to_csv(f"{Dir}All_significannt_trans_exposure_AfterQC_LDclumping_NoMHC_Unique.csv")
             os.system(f"cp {pqtltype}_{name}_{rscript} {pqtltype}_{name}_{r_prefix}_MRAnalysis_sbatch_command.sh {Dir}")
