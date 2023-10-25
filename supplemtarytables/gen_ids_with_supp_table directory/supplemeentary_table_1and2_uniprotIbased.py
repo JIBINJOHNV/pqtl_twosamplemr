5b48bcdeb93dd46f7a37578a151baeb05879db0d
JIBINJOHNV <69025183+JIBINJOHNV@users.noreply.github.com>
2023-10-25 09:37:09 -0400

    Update supplemeentary_table_1and2_uniprotIbased.py

--- supplemtarytables/gen_ids_with_supp_table directory/supplemeentary_table_1and2_uniprotIbased.py
+++ supplemtarytables/gen_ids_with_supp_table directory/supplemeentary_table_1and2_uniprotIbased.py
@@ -254,7 +254,9 @@ result_df.to_csv(f"/Users/jibinjohn/Desktop/MR_Analysis_Results_with_SCZ_NoUKB_C
 
 
 
-result_2df=result_df[['Approved symbol','Outcome','CIS/TRANS','Estimate_Direction','Lowest_Pvalue','DECODE_Estimate','UKBB_PPP_Estimate','CHR', 'Gene_start', 'Gene_end']].drop_duplicates()
+###########---------------------------------------------------------HUDSON PLOT ---------------------------------------------------------------###############
+
+result_2df=result_df[['Approved symbol','Outcome','CIS/TRANS','Estimate_Direction','Cateegary','Lowest_Pvalue','DECODE_Estimate','UKBB_PPP_Estimate','CHR', 'Gene_start', 'Gene_end']].drop_duplicates()
 result_2df["status"]=np.where(result_2df['Lowest_Pvalue']<0.00005,'significant',"not_significant")
 
 update_data2=[{'Approved symbol':'LGALS7_LGALS7B',"CHR":19,"Gene_start":38770968,"Gene_end":38773517}, 
@@ -283,3 +285,45 @@ result_2df_na=pd.merge(update_data2_df,result_2df_na,on="Approved symbol")
 result_2df=pd.concat([result_2df_notna,result_2df_na,])
 
 result_2df.to_csv("Decode_Biogen_CisExposure_TransExposureNoMHC_for_ploting.csv",index=None)
+
+
+result_2df['Outcome']=result_2df['Outcome'].replace({'Depression_iPSYCH_2023':'Depression', 'PGC3_SCZ_NoUKB':'SCZ', 'Cognition_Meta':'Cognition','BIP_PGC3_noukb':'BIP'})
+
+huston=result_2df[['Outcome','Approved symbol','CHR','Gene_start','Lowest_Pvalue','CIS/TRANS','Cateegary','Estimate_Direction','DECODE_Estimate','UKBB_PPP_Estimate']].drop_duplicates()
+huston=huston[ (huston['Estimate_Direction']!="-,+") & (huston['Estimate_Direction']!="+,-") ]
+huston=huston.sort_values(by=["Outcome","Approved symbol","CIS/TRANS","Lowest_Pvalue"])
+huston=huston.drop_duplicates(subset=['Outcome','Approved symbol','CIS/TRANS'],keep="first")
+
+huston=huston.rename(columns={'Outcome':'PHE','Approved symbol':'SNP','CHR':'CHR','Gene_start':'POS','Lowest_Pvalue':'pvalue','Cateegary':"Shape"})
+huston_cis=huston[huston['CIS/TRANS']=="CIS"]
+huston_trans=huston[huston['CIS/TRANS']!="CIS"]
+
+huston_cis.to_csv('Decode_Biogen_CisExposure_all_phenotype_hudson_input.csv',index=None)
+huston_trans.to_csv('Decode_Biogen_TransExposure_all_phenotype_hudson_input.csv',index=None)
+
+
+
+###
+result_3df=result_2df[ (result_2df['Estimate_Direction']!="-,+") & (result_2df['Estimate_Direction']!="+,-") ]
+result_3df_neg=result_3df[result_3df['Estimate_Direction'].isin(['-,NA', '-,-','NA,-']) ].sort_values(by=["Approved symbol","Outcome","CIS/TRANS","Lowest_Pvalue"])
+result_3df_pos=result_3df[result_3df['Estimate_Direction'].isin(['+,NA', '+,+','NA,+']) ].sort_values(by=["Approved symbol","Outcome","CIS/TRANS","Lowest_Pvalue"])
+
+
+
+result_3df_neg.sort_values(by=["Approved symbol","Outcome","CIS/TRANS","Lowest_Pvalue"])
+
+huston=result_3df_neg[['Outcome','Approved symbol','CHR','Gene_start','Lowest_Pvalue','CIS/TRANS','Estimate_Direction']].drop_duplicates()
+huston=huston.rename(columns={'Outcome':'PHE','Approved symbol':'SNP','CHR':'CHR','Gene_start':'POS','Lowest_Pvalue':'pvalue'})
+huston_cis=huston[huston['CIS/TRANS']=="CIS"]
+huston_trans=huston[huston['CIS/TRANS']!="CIS"]
+huston_cis.to_csv('Decode_Biogen_CisExposure_all_phenotype_hudson_input_negative.csv',index=None)
+huston_trans.to_csv('Decode_Biogen_TransExposure_all_phenotype_hudson_input_negative.csv',index=None)
+
+
+
+huston=result_3df_pos[['Outcome','Approved symbol','CHR','Gene_start','Lowest_Pvalue','CIS/TRANS','Estimate_Direction']].drop_duplicates()
+huston=huston.rename(columns={'Outcome':'PHE','Approved symbol':'SNP','CHR':'CHR','Gene_start':'POS','Lowest_Pvalue':'pvalue'})
+huston_cis=huston[huston['CIS/TRANS']=="CIS"]
+huston_trans=huston[huston['CIS/TRANS']!="CIS"]
+huston_cis.to_csv('Decode_Biogen_CisExposure_all_phenotype_hudson_input_positive.csv',index=None)
+huston_trans.to_csv('Decode_Biogen_TransExposure_all_phenotype_hudson_input_positive.csv',index=None)
