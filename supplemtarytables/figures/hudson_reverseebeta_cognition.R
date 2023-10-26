6bbf4bde8da1bf217f4477099fad3ea0f7e5043d
JIBINJOHNV <69025183+JIBINJOHNV@users.noreply.github.com>
2023-10-25 23:47:31 -0400

    Create hudson_reverseebeta_cognition.R

---
+++ supplemtarytables/figures/hudson_reverseebeta_cognition.R
@@ -0,0 +1,41 @@
+
+
+
+cis_df=read.csv('Decode_Biogen_CisExposure_all_phenotype_hudson_input.csv')
+trans_df=read.csv('Decode_Biogen_TransExposure_all_phenotype_hudson_input.csv')
+
+negative_cis_df <- cis_df %>%filter(Estimate_Direction %in% c('-,NA', '-,-', 'NA,-'))
+positive_cis_df <- cis_df %>%filter(Estimate_Direction %in% c('+,NA', '+,+', 'NA,+'))
+
+negative_cis_df_cog=negative_cis_df[negative_cis_df$PHE=="Cognition",]
+negative_cis_df_nocog=negative_cis_df[negative_cis_df$PHE!="Cognition",]
+
+positive_cis_df_cog=positive_cis_df[positive_cis_df$PHE=="Cognition",]
+positive_cis_df_nocog=positive_cis_df[positive_cis_df$PHE!="Cognition",]
+
+negative_cis_df<-rbind(negative_cis_df_nocog,positive_cis_df_cog)
+positive_cis_df<-rbind(positive_cis_df_nocog,negative_cis_df_cog)
+
+
+
+
+### All phenotype but cis and trans in separate figures
+negative_trans_df <- trans_df %>%filter(Estimate_Direction %in% c('-,NA', '-,-', 'NA,-'))
+positive_trans_df <- trans_df %>%filter(Estimate_Direction %in% c('+,NA', '+,+', 'NA,+'))
+
+phemirror(top=positive_cis_df, bottom =negative_cis_df, toptitle = "Positive Beta value",opacity=1,bottomtitle = "Negative Beta value",
+            annotate_p = c(0.00001, 0.00001),file = "cis_all_phenotype",)
+
+phemirror(top=positive_trans_df, bottom =negative_trans_df, toptitle = "Positive Beta value",opacity=1,bottomtitle = "Negative Beta value",
+            annotate_p = c(0.00001, 0.00001),file = "trans_all_phenotype",)
+
+combined_df <- rbind(cis_df, trans_df)
+
+#Cognition
+cog_df=combined_df[combined_df$PHE=="Cognition", ]
+cog_df <- subset(cog_df, select = -PHE)
+colnames(cog_df)[colnames(cog_df) == "CIS.TRANS"] <- "PHE"
+negative_cog_df <- cog_df %>%filter(Estimate_Direction %in% c('-,NA', '-,-', 'NA,-'))
+positive_cog_df <- cog_df %>%filter(Estimate_Direction %in% c('+,NA', '+,+', 'NA,+'))
+phemirror(bottom=positive_cog_df, top =negative_cog_df,opacity=1,annotate_p = c(0.00001, 0.00001),
+            file = "cognition_cis_trans",toptitle = "Cognition Positive Beta value",bottomtitle = "Cognition Negative Beta value",)
