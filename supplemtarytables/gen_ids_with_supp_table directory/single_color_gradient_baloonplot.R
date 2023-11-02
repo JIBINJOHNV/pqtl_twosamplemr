798a7f070653982ba5dae1ffd82178c46a5abf85
JIBINJOHNV <69025183+JIBINJOHNV@users.noreply.github.com>
2023-11-02 00:14:49 -0400

    Update single_color_gradient_baloonplot.R

--- supplemtarytables/gen_ids_with_supp_table directory/single_color_gradient_baloonplot.R
+++ supplemtarytables/gen_ids_with_supp_table directory/single_color_gradient_baloonplot.R
@@ -1,16 +1,30 @@
+
+
+library(dplyr)
+
+
 ## cis positive direction
 cis_Positive=df_filtered[ (df_filtered$`CIS.TRANS`=="CIS" &df_filtered$`Direction`=="+"), ] # TRANS ,CIS
 
 df_defined<-cis_Positive
 out_prefix<-deparse(substitute(df_defined))
-pqtl_type="CIS"
+pqtl_type="CIS" # CIS, TRANS
 Direction="Positive" # "Positive", "Negative"
 # Load your data and preprocess it
 
-p <- ggplot(df_defined, aes(x = Phenotype, y = Gene, color = P.value, shape = shape_vale)) +
-  geom_point(size = 3) +
-  scale_color_gradient(low = "light blue", high = "dark blue") +
-  labs(title = glue("{pqtl_type} PQTLs Beta {Direction}")) + theme_linedraw()
+# Define the shapes and their order
+shapes <- c("nominally significant" = 21, "FDR" = 22, "strictly significant" = 24)
+
+# Add a new column "shape" with the shapes based on "shape_vale" values
+df_defined <- df_defined %>% 
+  mutate(shape = factor(shape_vale, levels = c("nominally significant", "FDR", "strictly significant")))
+
+p <- ggplot(df_defined, aes(x = Phenotype, y = Gene)) +
+  geom_point(size = 3, aes(fill = P.value, shape = shape), stroke = 0.2,color="black") +
+    labs(title = glue("{pqtl_type} PQTLs Beta {Direction}"))+
+  scale_shape_manual(values = shapes) +
+  scale_fill_gradient(low = "#f1f1f6", high = "#0707f8") +  # Use a gradient color scale
+  theme_linedraw()+
   theme(axis.text.y = element_text(size = 8, face = "bold"),
         axis.text.x = element_text(size = 8, face = "bold")) +
   theme(legend.key.size = unit(1, "cm"),
