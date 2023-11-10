5addee5f7fcfa6840bf9b71eeb22c2a80feba9bc
JIBINJOHNV <69025183+JIBINJOHNV@users.noreply.github.com>
2023-11-10 01:42:36 -0500

    Update baloonplot_filee_preparation_v2.py

--- supplemtarytables/gen_ids_with_supp_table directory/baloonplot_filee_preparation_v2.py
+++ supplemtarytables/gen_ids_with_supp_table directory/baloonplot_filee_preparation_v2.py
@@ -1,5 +1,8 @@
 
 import pandas as pd 
+import numpy as np
+import math
+
 
 def to_scientific(num):
     return '{:.2e}'.format(num)
@@ -22,34 +25,47 @@ df2=df2.drop_duplicates(subset=["Gene symbol","CIS/TRANS","Outcome"],keep="first
 conditions = [
     ( (df2["BONF_P_Value"] < 0.05) & df2["BONF_P_Value"].notna()),
     ((df2["BONF_P_Value"] > 0.05) & (df2["FDR_P_value"] < 0.05)  & df2["FDR_P_value"].notna()  & df2["BONF_P_Value"].notna()),
-    ((df2["FDR_P_value"] > 0.05) & (df2['Lowest_Pvalue'] < 0.01) & df2["FDR_P_value"].notna()  & df2["BONF_P_Value"].notna())
+    ((df2["FDR_P_value"] > 0.05) & (df2['Lowest_Pvalue'] < 0.05) & df2["FDR_P_value"].notna()  & df2["BONF_P_Value"].notna())
 ]
 
 values=["strictly significant","FDR","nominally significant"]
 df2["shapee_vale"]=np.select(conditions, values, default='Unknown')
 
-df3=df2.pivot(index=['Gene symbol','CIS/TRANS','Estimate_Direction'], columns='Outcome', values=['Lowest_Pvalue',"shapee_vale","BONF_P_Value","FDR_P_value"]).reset_index()
+df3=df2.pivot(index=['Gene symbol','CIS/TRANS'], columns='Outcome', values=['Lowest_Pvalue',"shapee_vale","BONF_P_Value","FDR_P_value","Estimate_Direction"]).reset_index()
+
 
 df3.columns = ['_'.join(col).strip() for col in df3.columns.values]
 
 fdr_columns=['FDR_P_value_BIP', 'FDR_P_value_COG', 'FDR_P_value_MDD', 'FDR_P_value_SCZ']
 bnf_columns=['BONF_P_Value_BIP', 'BONF_P_Value_COG', 'BONF_P_Value_MDD', 'BONF_P_Value_SCZ']
 bnf_df = df3[(df3[bnf_columns] < 0.05).any(axis=1)]
-bnf_05_m1=pd.melt(bnf_df,id_vars=['Gene symbol_', 'CIS/TRANS_','Estimate_Direction_'],
+
+
+
+bnf_05_m1=pd.melt(bnf_df,id_vars=['Gene symbol_', 'CIS/TRANS_'],
                         value_vars=['Lowest_Pvalue_BIP', 'Lowest_Pvalue_COG', 'Lowest_Pvalue_MDD', 'Lowest_Pvalue_SCZ'])
 bnf_05_m1=bnf_05_m1.rename(columns={"value":"P-value","variable":"Phenotype"})
 bnf_05_m1["Phenotype"]=bnf_05_m1["Phenotype"].str.replace("Lowest_Pvalue_","")
 
 
-bnf_05_m2=pd.melt(bnf_df,id_vars=['Gene symbol_', 'CIS/TRANS_','Estimate_Direction_'],
+bnf_05_m2=pd.melt(bnf_df,id_vars=['Gene symbol_', 'CIS/TRANS_'],
                         value_vars=['shapee_vale_BIP', 'shapee_vale_COG', 'shapee_vale_MDD', 'shapee_vale_SCZ'])
 bnf_05_m2=bnf_05_m2.rename(columns={"value":"shape_vale","variable":"Phenotype"})
 bnf_05_m2["Phenotype"]=bnf_05_m2["Phenotype"].str.replace("shapee_vale_","")
 
-bnf_05_m=pd.merge(bnf_05_m1,bnf_05_m2,on=['Gene symbol_', 'CIS/TRANS_','Phenotype','Estimate_Direction_'])
+
+bnf_05_m3=pd.melt(bnf_df,id_vars=['Gene symbol_', 'CIS/TRANS_'],
+                        value_vars=['Estimate_Direction_BIP','Estimate_Direction_COG', 'Estimate_Direction_MDD','Estimate_Direction_SCZ'])
+bnf_05_m3=bnf_05_m3.rename(columns={"value":"Estimate_Direction","variable":"Phenotype"})
+bnf_05_m3["Phenotype"]=bnf_05_m3["Phenotype"].str.replace("Estimate_Direction_","")
+
+
+bnf_05_m=pd.merge(bnf_05_m1,bnf_05_m2,on=['Gene symbol_', 'CIS/TRANS_','Phenotype'])
+bnf_05_m=pd.merge(bnf_05_m,bnf_05_m3,on=['Gene symbol_', 'CIS/TRANS_','Phenotype'],how="outer")
 bnf_05_m=bnf_05_m[~bnf_05_m["shape_vale"].isna()]
 bnf_05_m=bnf_05_m.rename(columns={"Gene symbol_":"Gene","CIS/TRANS_":"CIS/TRANS",'Estimate_Direction_':'Estimate_Direction'})
 bnf_05_m['P-value'] = bnf_05_m['P-value'].apply(lambda x: -math.log10(x))
-bnf_05_m=bnf_05_m[bnf_05_m["P-value"]>=2]
+bnf_05_m=bnf_05_m[bnf_05_m["P-value"]>=1.300000]
+
 
 bnf_05_m.to_csv("Alll_pheeotype_cis_trans_bonf.csv",index=None)
